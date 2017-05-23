9d675de84ceedf03f5c54203a418593c86676718
BuildTools <if16b030@technikum-wien.at>
2017-05-23 17:00:46 +0200

    Removed comments

--- robot_gastontarry.cpp
+++ robot_gastontarry.cpp
@@ -37,69 +37,43 @@ void GastonTarry::doStep() {
     if(maze->isCrossing(coords)){
         if(map[left.posx][left.posy] == 0 &&  map[right.posx][right.posy] == 0 && map[forward.posx][forward.posy] == 0){
             map[backwards.posx][backwards.posy] = BACK;
-            std::cout << "set back on x:" << backwards.posx  << " y:" << backwards.posy << std::endl;
         }
-        std::cout << "kreuzung" << std::endl;
-        std::cout << map[left.posx][left.posy] << std::endl;
-        std::cout << map[right.posx][right.posy] << std::endl;
-        std::cout << map[forward.posx][forward.posy] << std::endl;
         if(maze->getPosition(left) == ' ' && map[left.posx][left.posy] != STOP && map[left.posx][left.posy] != BACK){
-            std::cout << "links" << std::endl;
             turn(LEFT);
             coords = left;
             map[coords.posx][coords.posy] = STOP;
-            std::cout << "set stop on x:" << coords.posx  << " y:" << coords.posy << std::endl;
         }else if(maze->getPosition(right) == ' ' && map[right.posx][right.posy] != STOP && map[right.posx][right.posy] != BACK){
-            std::cout << "rechts" << std::endl;
             turn(RIGHT);
             coords = right;
             map[coords.posx][coords.posy] = STOP;
-            std::cout << "set stop on x:" << coords.posx  << " y:" << coords.posy << std::endl;
         }else if(maze->getPosition(forward) == ' ' && map[forward.posx][forward.posy] != STOP && map[forward.posx][forward.posy] != BACK){
-            std::cout << "gerade" << std::endl;
             coords = forward;
             map[coords.posx][coords.posy] = STOP;
-            std::cout << "set stop on x:" << coords.posx  << " y:" << coords.posy << std::endl;
         }else if(maze->getPosition(left) == ' ' && map[left.posx][left.posy] == BACK){
-            std::cout << "links" << std::endl;
             turn(LEFT);
             coords = left;
             map[coords.posx][coords.posy] = STOP;
-            std::cout << "set stop on x:" << coords.posx  << " y:" << coords.posy << std::endl;
         }else if(maze->getPosition(right) == ' ' && map[right.posx][right.posy] == BACK){
-            std::cout << "rechts" << std::endl;
             turn(RIGHT);
             coords = right;
             map[coords.posx][coords.posy] = STOP;
-            std::cout << "set stop on x:" << coords.posx  << " y:" << coords.posy << std::endl;
         }else if(maze->getPosition(forward) == ' ' && map[forward.posx][forward.posy] == BACK){
-            std::cout << "gerade" << std::endl;
             coords = forward;
             map[coords.posx][coords.posy] = STOP;
-            std::cout << "set stop on x:" << coords.posx  << " y:" << coords.posy << std::endl;
         }
     }else{
-        std::cout << "keine kreuzung " << std::endl;
         if(maze->getPosition(left) == ' '){
-            std::cout << "links 2" << std::endl;
             turn(LEFT);
             coords = left;
-
         }else if(maze->getPosition(right) == ' '){
-            std::cout << "rechts 2" << std::endl;
             turn(RIGHT);
             coords = right;
-
         }else if(maze->getPosition(forward) == ' '){
-            std::cout << "gerade 2" << std::endl;
             coords = forward;
-
         }else{
-            std::cout << "drehung 2" << std::endl;
             turn(LEFT);
             turn(LEFT);
             coords = calculateNextPos(FORWARD);
         }
     }
-    std::cout << std::endl << std::endl;
 }
