a888868a88cafb164f7bbdfbe8220bd2c8a853fd
Alexander Dietrich <alexanderdietrich@gmx.at>
2017-05-23 10:37:53 +0200

    read in file

--- maze.cpp
+++ maze.cpp
@@ -1,3 +1,30 @@
 /* Copyright Selina Brinnich & Alexander Dietrich */
 
+#include <fstream>
+#include <iostream>
+#include <string>
 #include "maze.h"
+
+
+void Maze::readFromFile(std::string filename) {
+    std::ifstream file;
+    ///open file
+    file.open(filename, std::ios::out);
+    char c;
+    int x = 0, y = 0;
+
+    ///go through file and read each character to put the maze into the vector
+    while (!file.eof()){
+        file.get(c);
+        if(c != '\n')
+        {
+            lab[x][y] = 'c';
+            x++;
+        }
+        ///if character is newline, go to next line in vector and reset x-axis
+        else{
+            x = 0;
+            y ++;
+        }
+    }
+}
